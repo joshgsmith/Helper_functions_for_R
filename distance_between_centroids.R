928d81d561ac9d9786a633b3757ee6bd581df7d3
Joshua G. Smith <joshsmith@nceas.ucsb.edu>
2022-07-27 17:11:27 -0700

    Update distance_between_centroids.R

--- distance_between_centroids.R
+++ distance_between_centroids.R
@@ -1,7 +1,8 @@
 #By Joshua G. Smith
 #July 27, 2022
 
-#This function calculates the distance between centroids in high dimensional space. 
+#This function calculates the distance between centroids in high dimensional space. It works best when year is the grouping variable of interest (distance from 
+#one year to the next). However, it can be applied to any grouping variable. 
 #Requires usedist::dist_between_centroids()
 #Replace 'year' with centroid groupings. 
 
@@ -30,24 +31,29 @@ cenfun <- function(group, c, x) {
 
 
 Example: 
-ccfrp <- cenfun(group=CCFRP_group_vars,c=CCFRP_group_vars$year, x=CCFRP_distmat)
 
-# A tibble: 13 × 3
-   centroid_1 centroid_2 distance
-   <chr>      <chr>         <dbl>
- 1 2007       2008         0.0908
- 2 2008       2009         0.141 
- 3 2009       2010         0.103 
- 4 2010       2011         0.157 
- 5 2011       2012         0.0595
- 6 2012       2013         0.181 
- 7 2013       2014         0.0949
- 8 2014       2015         0.100 
- 9 2015       2016         0.154 
-10 2016       2017         0.0975
-11 2017       2018         0.0867
-12 2018       2019         0.0921
-13 2019       2020         0.0654
+require(vegan)
+require(usedist)
+
+dune <- read.delim ('https://raw.githubusercontent.com/zdealveindy/anadat-r/master/data/dune2.spe.txt', row.names = 1)       #biological data
+dune.env <- read.delim ('https://raw.githubusercontent.com/zdealveindy/anadat-r/master/data/dune2.env.txt', row.names = 1)   #group vars
+
+data(dune) 
+data(dune.env)
+
+dune_dist <- vegdist(dune, method="bray")  #calculate distmat
+
+cenfun(dune.env,dune.env$Use,dune_dist) #apply centroid distance function
+
+# A tibble: 2 × 3
+  centroid_1 centroid_2 distance
+  <chr>      <chr>         <dbl>
+1 Hayfield   Haypastu      0.327
+2 Haypastu   Pasture       0.210
+
+
+
+
 
 
 
